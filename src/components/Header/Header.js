5ebcfe8885c07c17a35b93b172eb3a95b74bfd26
andrii-hutsko-xite <andrii.hutsko@xite.com>
2024-12-14 09:42:19 +0100

    header added + logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import "./Header.css";
+
+function Header() {
+    return (
+        <header>
+            <div className="content">
+                <div className="links">
+                    <a href="./">Delivery & Payment</a>
+                    <a href="./">Help</a>
+                    <a href="./">FAQ</a>
+                </div>
+                <div className="main">
+                    <img src="./media/logo.svg" alt='logo'/>
+                    <div className="controls">
+                        <input type="text" placeholder="Search" />
+                        <button>Log in</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
